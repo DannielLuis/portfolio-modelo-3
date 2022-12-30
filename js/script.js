63526d984fcd9b2b78381466fa93a2d09402f7c2
Daniel Luis <danniellcontato@gmail.com>
2022-12-30 20:57:17 -0300

    Testando botões da section sobre

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 window.addEventListener("load", () => {
     alert("Testando javascript")
 });*/
+
+const btn_sobre = document.getElementById("btn_sobre");
+const btn_cursos = document.getElementById("btn_cursos");
+
+btn_sobre.addEventListener("click", () => {
+    alert("Testando btn sobre")
+});
+
+btn_cursos.addEventListener("click", () => {
+    alert("Testando btn cursos")
+});
