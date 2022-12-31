3797786e88799563831618ff5e884e53a8c7b754
Daniel Luis <danniellcontato@gmail.com>
2022-12-31 06:11:34 -0300

    Funções dos botões

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 //
-/*
-window.addEventListener("load", () => {
-    alert("Testando javascript")
-});*/
-
 const btn_sobre = document.getElementById("btn_sobre");
 const btn_cursos = document.getElementById("btn_cursos");
+const sobre = document.querySelector(".conteiner_sobre");
+const cursos = document.querySelector(".conteiner_cursos");
 
 btn_sobre.addEventListener("click", () => {
-    alert("Testando btn sobre")
+    sobre.classList.add("ativo");
+    cursos.classList.remove("ativo");
 });
 
 btn_cursos.addEventListener("click", () => {
-    alert("Testando btn cursos")
+    sobre.classList.remove("ativo");
+    cursos.classList.add("ativo");
 });
