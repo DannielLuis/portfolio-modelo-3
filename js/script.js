88654ff23beed18330ef0cb5e63817b27d6242b5
Daniel Luis <danniellcontato@gmail.com>
2023-01-04 20:21:02 -0300

    Justando a font da descrição da section sobre

--- js/script.js
+++ js/script.js
@@ -12,17 +12,6 @@ const btn_cursos = document.getElementById("btn_cursos");
 const sobre = document.querySelector(".conteiner_sobre");
 const cursos = document.querySelector(".conteiner_cursos");*/
 
-
-window.onload = function(){
-    location.href = "index.html#sobre";
-    updateUrl(dataUrl)
-}
-/*
-window.addEventListener("load", () => {
-    //alert("Testando")
-    location.href = "index.html#projetos";
-});*/
-
 //console.log(html1.scrollTop);
 
 function myscroll(){
@@ -45,6 +34,18 @@ function btnClick(){
     btn_hamburger.classList.toggle("clicked");
     menu_mobile.classList.toggle("active_menu");
 }
+
+/*
+window.onload = function(){
+    location.href = "index.html#sobre";
+    updateUrl(dataUrl)
+}*/
+
+window.addEventListener("load", () => {
+    location.href = "index.html#sobre";
+    //location.href = "index.html#projetos";
+    updateUrl(dataUrl)
+});
 
 btn_hamburger.addEventListener("click", btnClick);
 
